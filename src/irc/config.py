d8dd0ba27a7ab5995f6f4f2b7ee6e88a4ba31ead
redwire <redwire@riseup.net>
2013-08-17 03:21:22 -0230

    Simple config file added

---
+++ src/irc/config.py
@@ -0,0 +1,17 @@
+# Name of the database file to store reminders and notifications in.
+database = "relae.db"
+
+# IRC server to connect to on startup.
+server = "irc.rizon.net"
+
+# Port to connect to the server via.
+portno = 6667
+
+# Channel for the bot to join.
+channel = "relaetest"
+
+# Nickname assumed by the bot for communication and command.
+nickname = "Reminderbot"
+
+# Password for a registered nick.
+password = ""
