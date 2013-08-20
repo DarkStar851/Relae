e4473ae50a04a6f2fea377ce176a050870d22943
Scott Stamp <scottstamp851@gmail.com>
2013-08-20 20:32:09 -0230

    Update config.py
    Restored original

--- src/irc/config.py
+++ src/irc/config.py
@@ -1,8 +1,24 @@
-database = 'relae.db'
-server = 'irc.freenode.net
+## This file will be dynamically replaced by the start script ##
+# Name of the database file to store reminders and notifications in.
+database = "relae.db"
+
+# IRC server to connect to on startup.
+server = "irc.rizon.net"
+
+# Port to connect to the server via.
 portno = 6667
-channel = 'relaetest'
-nickname = 'Dergs'
-password = ''
-relae_ip = '127.0.0.1'
+
+# Channel for the bot to join.
+channel = "relaetest"
+
+# Nickname assumed by the bot for communication and command.
+nickname = "Reminderbot"
+
+# Password for a registered nick.
+password = ""
+
+# IP for the Relae server.
+relae_ip = ""
+
+# Port to connect to the Relae server over.
 relae_port = 9001
