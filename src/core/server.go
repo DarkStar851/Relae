f4b430c28114266e1efb6232d3ecde13fc36245d
redwire <redwire@riseup.net>
2013-09-04 13:32:09 -0230

    Cleaned things up

--- src/core/server.go
+++ src/core/server.go
@@ -28,72 +28,27 @@ type Command struct {
 }
 
 type Request struct {
-    InterfaceName string
-    UniqueID int64
-    Responses chan string
     Cmd Command
+    Response chan string
 }
 
-var KILLFLAG bool = false
-
-func ParseRequest(iname, msg string, uid int64, resp chan string) Request {
+func ParseRequest(msg string, res chan string) Request {
     p := strings.Split(msg)
     i1 := strconv.ParseInt(p[3], 10, 64)
     i2 := strconv.ParseInt(p[4], 10, 64)
-    return Request{iname, uid, resp, Command{p[0], p[1], p[2], i1, i2, p[5]}}
+    return Request{Command{p[0], p[1], p[2], i1, i2, p[5]}, res}
 }
 
-func CreateIDGenerator(start int64) (func () int64) {
-    return func () int64 {
-        start += 1
-        return start
-    }
+type Worker struct {
+    Input *net.TCPConn
+    Output *net.TCPConn
+    Requests chan Request
+    Responses chan string
 }
 
-func HandleRequests(dbdriver, dbfilename string, req chan Request, dead *bool) {
-    db, err = sql.Open(dbdriver, dbfilename)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer db.Close()
-    db.Exec("create table if not exists reminders " +\
-            "(src text, dest text, created float, date float, msg text)")
-    db.Exec("create table if not exists notifications " +\
-            "(src text, dest text, created float, msg text)")
-    tx, _ = db.Begin()
-    for !*dead {
-        r := <-req
-        dispatchFn, err := dispatch.Mapping[r.FunctionName]
-        if err != nil {
-            log.Println("No dispatch function for " + r.FunctionName)
-            continue
-        }
-        r.Responses <- dispatchFn(r.Cmd, tx)
-    }
-    tx.Commit()
-}
-
-func IssueReminders(iname string, sleepSec time.Duration, req chan Request, dead *bool) {
-    for !*dead {
-        time.Sleep(sleepSec)
-        // Create reminder request and communicate result.
-    }
-}
-
-func ReadFromInterface(iname string, in *net.TCPConn, req chan Request, dead *bool) {
-    buffer := make([]byte, 1024)
-    quit := []byte(QUIT_MSG)
-    for !(*dead || bytes.Equal(buffer, quit)) {
-        n, err := in.Read(buffer)
-        if err != nil {
-            break
-        }
-
-    }
+func (w *Worker) StartWorking() {
+    
 }
 
 func main() {
-    idGen := CreateIDGenerator(0)
-    fmt.Println(idGen())
-    fmt.Println(idGen())
 }
